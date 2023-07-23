7a2285a9c3aa745d829452bec3cf707baf021dd2
Andrew Ford <andrew@andrewford.co.nz>
2023-07-23 07:06:37 +0000

    Added table to list component

--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,17 @@ import React from 'react';
 import theme from '../theme/themeConfig';
 import { Layout, Space } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
-
+import List from "../components/List";
 
 const HomePage = () => (
   <ConfigProvider theme={theme}>
     <div className="App">
       <Layout>
-        <Header>
+        <Header style={{color: '#fff'}}>
           My Apps name
         </Header>
         <Content>
-          
+          <List />
         </Content>
         <Footer>
           &copy; Copyright 2023

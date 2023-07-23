7d61f56b8413ce0df2f53d2936ba5db11f8c7e2d
Andrew Ford <andrew@andrewford.co.nz>
2023-07-23 05:46:00 +0000

    Adde Layout to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
+'use client'
 import { Button, ConfigProvider } from 'antd';
 import React from 'react';
 import theme from '../theme/themeConfig';
+import { Layout, Space } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
 
 const HomePage = () => (
   <ConfigProvider theme={theme}>
     <div className="App">
-      <></>
+      <Layout>
+        <Header>
+          Header
+        </Header>
+        <Content>
+          Content
+        </Content>
+        <Footer>
+          Footer
+        </Footer>        
+      </Layout>
     </div>
   </ConfigProvider>
 );
